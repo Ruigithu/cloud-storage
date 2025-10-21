e91f50e6e0844813d5c3e27d957757f7342c3a4b
Rui Peng <162542860+Ruigithu@users.noreply.github.com>
2025-10-21 01:26:55 +0100

    Update API URL in authService.js

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -2,7 +2,7 @@ import {ERROR_MESSAGES} from "../utils/validators.js";
 import apiRequest from "../utils/apiRequest";
 
 const getApiUrl = () => {
-    return process.env.REACT_APP_API_URL || 'http://localhost:8080';
+    return process.env.REACT_APP_API_URL || 'https://api.ruis-cloud-storage.work';
 };
 
 export const API_ENDPOINTS = {
